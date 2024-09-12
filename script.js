2e8e8bd31f356c0e79bacac6f79353665ede6e89
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2024-09-12 12:21:21 -0300

    tarefas lista de tarefas 1, 2, 3, 4
    feito as 4 tarefas. Infelizmente, tive que mudar o método realizado.

--- script.js
+++ script.js
@@ -10,3 +10,83 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+const li = document.getElementsByClassName("task__item");
+const ul = document.querySelector(".tasks__list");
+const form = document.querySelector(".form__container");
+ul.removeChild(li[0]);
+ul.removeChild(li[0]);
+ul.removeChild(li[0]);
+
+function renderElements(task){
+  var codes = [];
+  for(i=0; i<task.length; i++){
+    let a = createTaskItem(task[i].title, task[i].type);
+    codes.push(a);
+  };
+  for(o=0; o<codes.length; o++){
+    ul.appendChild(codes[o])
+  };
+  return ul;
+}
+
+renderElements(tasks)
+function createTaskItem(ti, ty){
+
+  const liJs = document.createElement("li");
+  const spanJs = document.createElement("span");
+  const divJs = document.createElement("div");
+  const pJs = document.createElement("p");
+  const buttonJs = document.createElement("button");
+  buttonJs.addEventListener("click", function (event){
+    for(i=0; i<tasks.length; i++){
+      if(tasks[i].title === ti){
+        liJs.remove();
+        return 
+      }
+    };
+    for(i=0; i<arrValores.length; i++){
+      if(arrValores[i].title === ti){
+        liJs.remove();
+        return
+      }
+    };
+  })
+  
+  liJs.classList.add("task__item");
+  divJs.classList.add("task-info__container");
+  buttonJs.classList.add("task__button--remove-task");
+  if(ty.toLowerCase() === "urgente"){
+    spanJs.classList.add("task-type")
+    spanJs.classList.add("span-urgent")
+  } else if(ty.toLowerCase() === "importante"){
+    spanJs.classList.add("task-type")
+    spanJs.classList.add("span-important")
+  } else if(ty.toLowerCase() === "normal"){
+    spanJs.classList.add("task-type")
+    spanJs.classList.add("span-normal")
+  };
+  
+  pJs.innerText = ti;
+  
+  liJs.appendChild(divJs);
+  liJs.appendChild(buttonJs);
+  divJs.appendChild(spanJs);
+  divJs.appendChild(pJs);
+  
+  return liJs;
+}
+
+const arrValores = [];
+form.addEventListener("submit", function(event) {
+  event.preventDefault()
+  
+  const input = document.querySelector(".form__input--text");
+  const select = document.querySelector(".form__input--priority");
+  const valores = {title: input.value ,type: select.value }
+  arrValores.push(valores)
+  tasks.push(valores)
+
+  renderElements(arrValores)
+  arrValores.pop()
+}
+)
